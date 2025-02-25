e2e082706d876590e049d46f8bc200da380b73af
Pradnesh Patil <122784285+Patilpradnesh@users.noreply.github.com>
2025-02-25 22:08:34 +0000

    datatype done

---
+++ Basic-1/02-dataType.js
@@ -0,0 +1,20 @@
+// "use strict"; // it use for treat all js code as newer version
+
+// alert("hello");  // we are using node js so its not working here , it only work on browser
+
+// string data type 
+let name ="string data type"
+// numeric
+let number= 123;
+// boolean
+let istruedata='true'
+// null : its standalon value 
+// undefined : varable is defined but value is not defined yet
+// symbol for finding uniqueness
+// object 
+
+console.log(typeof "string")
+console.log(typeof 23)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
