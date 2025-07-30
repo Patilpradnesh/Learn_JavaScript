c1bbd51919cc4f1bd577884e74f2f3b3aa2a6cfd
patilpradnesh <patilpradnesh4124@gmail.com>
2025-07-30 15:22:41 +0530

    learning object related loop concepts in js

---
+++ 05-loops/eachLoop.js
@@ -0,0 +1,53 @@
+const coding =['javascript','c++','ruby','swift','python'];
+// first variation
+coding.forEach(function (item){
+    // console.log(item); // it will print each item in the array
+})
+
+//second variation with arrow function
+coding.forEach((item)=>{
+    // console.log(item); // it will print each item in the array
+})
+
+
+// array name  foreach method in side it use call back function  with item as parameter (it is callback function so it doesn't have name )
+// forEach method is used to iterate through each element of the array
+// it is not a loop but it is used to iterate through array elements
+
+// suppose we have already written function and we want to perform the forEach loop on that function 
+let i=0;
+function printMe(item){
+    // console.log(`${i} is ${item}`);
+    i++;
+}
+
+// coding.forEach(printMe); // it will print each item in the array using the printMe function
+
+coding.forEach((item,index,arr)=>{
+    // console.log(item,index,arr);
+    
+})
+
+// we have arr and object are present in it 
+const myCoding =[
+    {
+    languageName:"javaScript",
+    languageFile:"js"
+},
+    {
+    languageName:"java",
+    languageFile:".java"
+},
+    {
+    languageName:"C++",
+    languageFile:"cpp"
+},
+    {
+    languageName:"Ruby",
+    languageFile:"r"
+}
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+})
--- 05-loops/object.js
+++ 05-loops/object.js
@@ -13,7 +13,32 @@ for(const key in object){
     // for access the values we use name of object with key parameter
     // console.log(object[key]);
     // combine representation is like this 
-    console.log(`${key} short cut of ${object[key]}`);
+    // console.log(`${key} short cut of ${object[key]}`);
 
 
+}
+
+
+// is that this for in loop work on array also
+
+const programming=["js","cpp","java","ruby","c#"];
+
+for (const key in programming){
+    // console.log(key); // it give key of array (array has key start with zero and bay default it is number );
+   // console.log(programming[key]); // it give value of array
+    // console.log(`${key} is a key for the array object ${programming[key]}`); // it give combined representation
+}
+
+//  can we use for in loop on map function ;
+
+const map =new Map();
+map.set('01',"bmw")
+map.set('02',"Audi")
+map.set('03',"Mercedes")
+map.set('04','Tata')
+map.set('05',"mahendra")
+
+for(const [key,value] in map){
+    console.log(key); // TypeError: map is not iterable
+    console.log(value);
 }
